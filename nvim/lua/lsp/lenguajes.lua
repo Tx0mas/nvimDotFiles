4eec38168a16e5d35faee83b9a129ddf0a85919a
Tx0mas <thom@DESKTOP-5B9OOUA.localdomain>
2026-02-15 03:02:29 -0300

    mason.. me aburri del lsp built in

--- nvim/lua/lsp/lenguajes.lua
+++ nvim/lua/lsp/lenguajes.lua
@@ -1,4 +1,30 @@
+-- 1. Configuración básica de Mason
+require("mason").setup({
+    ui = {
+        border = "rounded",
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
+})
+
+-- 2. El "puente" mágico: mason-lspconfig
+require("mason-lspconfig").setup({
+    -- Esta lista asegura que se instalen apenas abras Neovim
+    ensure_installed = { 
+        "clangd",    -- C/C++
+        "lua_ls",    -- Lua
+        "pyright",   -- Python
+        "tinymist",  -- Typst
+        "gopls"      -- Go
+    },
+})
+
+-- 3. Habilitar los servicios (Tu configuración original + Go)
 vim.lsp.enable("clangd")
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("pyright")
 vim.lsp.enable("tinymist")
+vim.lsp.enable("gopls")
