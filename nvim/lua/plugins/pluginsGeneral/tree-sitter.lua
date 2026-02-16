954d49b3e9de1c5aafe52c30908537160cd18739
Tx0mas <thom@DESKTOP-5B9OOUA.localdomain>
2026-02-15 23:35:57 -0300

    treesittergo

--- nvim/lua/plugins/pluginsGeneral/tree-sitter.lua
+++ nvim/lua/plugins/pluginsGeneral/tree-sitter.lua
@@ -6,10 +6,10 @@ return{
         require'nvim-treesitter'.setup {
           install_dir = vim.fn.stdpath('data') .. '/site'
         }
-        require'nvim-treesitter'.install { 'cpp', 'c' , 'lua', 'python'}
+        require'nvim-treesitter'.install { 'cpp', 'c' , 'lua', 'python', 'go'}
 
         vim.api.nvim_create_autocmd("FileType", {
-          pattern = { "c", "cpp" , "lua", "python"},
+          pattern = { "c", "cpp" , "lua", "python",'go'},
           callback = function(args)
             vim.treesitter.start(args.buf)
           end,
